7a15966dddec94bfb1f5add3becc4f22c7b35b4d
N I G H T F A L L <116102438+IttsmeNightfall@users.noreply.github.com>
2024-11-20 21:58:25 +0000

    Finalizando Login

---
+++ static/js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const togglePassword = document.querySelector('.toggle_password');
+    const passwordInput = document.querySelector('#senha');   
+    const passwordIcon = document.querySelector('.password_icon');
+
+    // Obtenha os caminhos das imagens a partir dos atributos data-
+    const eyeIcon = togglePassword.getAttribute('data-eye');
+    const eyeSlashIcon = togglePassword.getAttribute('data-eye-slash');
+
+    let isPasswordVisible = false; // Inicializa a variável
+
+    togglePassword.addEventListener('click', function() {
+        
+        const type = passwordInput.type === 'password' ? 'text' : 'password';
+        passwordInput.type = type;
+        
+        isPasswordVisible = !isPasswordVisible;
+       
+        passwordIcon.setAttribute('src', isPasswordVisible ? eyeSlashIcon : eyeIcon);
+
+        passwordIcon.setAttribute('alt', isPasswordVisible ? 'Ocultar senha' : 'Mostrar senha');
+    });
+});
