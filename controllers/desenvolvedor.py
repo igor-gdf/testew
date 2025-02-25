ebc2c41e1016c7b1980de00949ffe85f126c8b25
igorDeveloper <138522037+igor-gdf@users.noreply.github.com>
2025-02-25 01:10:39 +0000

    flask login em desenvolvedor.py

--- controllers/desenvolvedor.py
+++ controllers/desenvolvedor.py
@@ -1,16 +1,16 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from models.Jogo import Jogo
 from utils import db
 from functools import wraps
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 dev_bp = Blueprint('dev', __name__, url_prefix='/dashboard')
 
 def developer_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-
-        if session.get('funcao') != 'desenvolvedor':
+        # Verificando a função do usuário com current_user
+        if current_user.funcao != 'desenvolvedor':
             flash('Acesso restrito: somente desenvolvedores podem enviar jogos.', 'danger')
             return redirect(url_for('dashboard'))
         return f(*args, **kwargs)
@@ -20,7 +20,7 @@ def developer_required(f):
 @login_required
 @developer_required
 def criar_jogos():
-    if 'usuario' not in session:
+    if current_user is None:  # Verificando se o usuário está logado
         flash('Você precisa estar logado para acessar esta página.', 'danger')
         return redirect(url_for('login'))
     
@@ -33,7 +33,7 @@ def criar_jogos():
             flash("Preencha todos os campos obrigatórios!", "danger")
             return redirect(url_for('dev.criar_jogos'))
         
-        id_usuario = session.get('id_usuario')
+        id_usuario = current_user.id  # Usando current_user.id para pegar o ID do usuário
         
         if not id_usuario:
             flash("Usuário não autenticado corretamente.", "danger")
